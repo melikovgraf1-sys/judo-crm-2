167a8f7a10966aafe748592eb91db5967b20057f
melikovgraf1-sys <melikovgraf1@gmail.com>
2025-09-17 12:58:14 +0300

    s

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig: FirebaseOptions = {
 
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAulX_16s1hU8Y-WT0IaWQmmoZJhr_0Xy0",
+  authDomain: "precise-slice-397909.firebaseapp.com",
+  projectId: "precise-slice-397909",
+  storageBucket: "precise-slice-397909.firebasestorage.app",
+  messagingSenderId: "952584870116",
+  appId: "1:952584870116:web:4d801cf061511d8c5934f1",
 };
 const app: FirebaseApp | undefined = Object.values(firebaseConfig).every(Boolean)
   ? initializeApp(firebaseConfig)
